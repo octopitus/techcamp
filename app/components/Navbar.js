dcaf54e99196ec78ce27e7385b57ebb32116807e
Luy Tran <octopitus@gmail.com>
2015-07-22 22:08:56 +0700

    change order of navigation links

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -19,15 +19,15 @@ export default class Navbar extends React.Component {
 		return (
 			<div className={"navbar mdl-layout__header mdl-layout__header--waterfall" + scrolling}>
 			  <div className="mdl-layout__header-row">
-			    <span className="navbar-title mdl-layout-title">Techcamp Saigon Uni 2015</span>
+			  	<a href="/"><span className="navbar-title mdl-layout-title">Techcamp Saigon Uni 2015</span></a>
 			    <div className="navbar-spacer mdl-layout-spacer"></div>
 			    <div className="navbar-container">
 			      <nav className="navbar-nav mdl-navigation">
 			      	<NavLink to="about" smooth={true} offset={-28} duration={500} text="About" handleClick={this.handleClick.bind(this, 'about')} active={this.state.active} />
-			      	<NavLink to="submit" smooth={true} offset={-28} duration={500} text="Submit topic" handleClick={this.handleClick.bind(this, 'submit')} active={this.state.active} />
-			      	<NavLink to="purchase" smooth={true} offset={-28} duration={500} text="Purchase ticket" handleClick={this.handleClick.bind(this, 'purchase')} active={this.state.active} />
 			      	<NavLink to="topic" smooth={true} offset={-28} duration={500} text="Hot topics" handleClick={this.handleClick.bind(this, 'topic')} active={this.state.active} />
+			      	<NavLink to="submit" smooth={true} offset={-28} duration={500} text="Submit topic" handleClick={this.handleClick.bind(this, 'submit')} active={this.state.active} />
 			      	<NavLink to="memory" smooth={true} offset={-28} duration={500} text="Photos" handleClick={this.handleClick.bind(this, 'memory')} active={this.state.active} />
+			      	<NavLink to="purchase" smooth={true} offset={-28} duration={500} text="Purchase ticket" handleClick={this.handleClick.bind(this, 'purchase')} active={this.state.active} />
 			      </nav>
 			    </div>
 			  </div>
